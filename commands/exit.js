56c276357b002e361286f9ab77fc005de8e2cd32
joat28 <prabhat.rao@iitg.ac.in>
2021-03-09 12:35:34 +0530

    add Youtube music (link + text) - play and exit commands

---
+++ commands/exit.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const { prefix } = require("../config.json");
+module.exports = {
+	name: "exit",
+	description: `request to exit a voice channel`,
+	aliases: ["e"],
+	args: false,
+	guildOnly: true,
+	usage: `\`${prefix}exit\``,
+	async execute(msg) {
+        msg.member.voice.channel.leave();
+	},
+};
